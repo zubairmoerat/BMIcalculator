3aee830ae79133699b6a1c7982c145727d1f2ffc
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-13 20:48:26 +0200

    finalDraft

--- script/code.js
+++ script/code.js
@@ -1,12 +1,12 @@
 document.getElementById('calculateButton').addEventListener('click', function () {
-    var weight = parseFloat(document.getElementById('weight').value);
-    var height = parseFloat(document.getElementById('height').value);
+    let weight = parseFloat(document.getElementById('weight').value);
+    let height = parseFloat(document.getElementById('height').value);
 
     if (!isNaN(weight) && !isNaN(height) && height > 0) {
-        var bmi = weight / ((height / 100) * (height / 100));
-        var resultElement = document.getElementById('result');
+        const bmi = weight / ((height / 100) * (height / 100));
+        const resultElement = document.getElementById('result');
 
-        var category;
+        let category;
         if (bmi < 18.5) {
             category = 'Underweight';
         } else if (bmi >= 18.5 && bmi < 25) {
@@ -15,7 +15,7 @@ document.getElementById('calculateButton').addEventListener('click', function ()
             category = 'Overweight';
         }
 
-        var comment;
+        let comment;
         if (bmi < 18.5) {
             comment = 'You need to eat more!!';
         } else if (bmi >= 18.5 && bmi < 25) {
